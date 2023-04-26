4600ea277bbfe4cf6724ac8283855ccfd3f9401a
talisd <dimat1@yahoo.com>
2023-04-26 11:30:03 -0400

    cidr

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port = "80"
   to_port = "80"
   protocol = "tcp"
-  cldr_block =["0.0.0.0/0"]
+  cidr_block =["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blob.id
 }
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port = "443"
   to_port = "443"
   protocol = "tcp"
-  cldr_block =["0.0.0.0/0"]
+  cidr_block =["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blob.id
 }
@@ -60,7 +60,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port = "0"
   to_port = "0"
   protocol = "-1"
-  cldr_block =["0.0.0.0/0"]
+  cidr_block =["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blob.id
 }
