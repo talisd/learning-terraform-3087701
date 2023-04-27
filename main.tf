557a7b9e427d0ea49360af788965d2b1ba98ed3c
talisd <dimat1@yahoo.com>
2023-04-27 12:53:29 -0400

    remove logs

--- main.tf
+++ main.tf
@@ -67,10 +67,6 @@ module "alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
-
   target_groups = [
     {
       name_prefix      = "blog-"
