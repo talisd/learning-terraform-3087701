9d5603cc2e62b814ea7e1128da2087016c26a24e
talisd <dimat1@yahoo.com>
2023-04-27 15:17:14 -0400

    add subnet

--- main.tf
+++ main.tf
@@ -46,6 +46,7 @@ module "blog_sg" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  subnet_id     = module.blog_vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
