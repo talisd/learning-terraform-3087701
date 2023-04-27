e35b1ddc29eeba7a773d3c2ce7368222a580c8b1
talisd <dimat1@yahoo.com>
2023-04-27 00:41:03 -0400

    More quotes

--- main.tf
+++ main.tf
@@ -36,10 +36,10 @@ module "blog_sg" {
 
   vpc_id = data.aws_vpc.default.id
 
-  ingress_rules       = [http-80-tcp, https-443-tcp]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules        = [http-80-tcp, https-443-tcp]
+  egress_rules        = ["http-80-tcp", "https-443-tcp"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
