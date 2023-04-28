cbd00d98ece4d1fca23e4aa52e3986204043ba19
talisd <dimat1@yahoo.com>
2023-04-28 13:44:37 -0400

    vars

--- variables.tf
+++ variables.tf
@@ -2,3 +2,45 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+
+variable "ami_filter" {
+
+  description = "Name filter and owner for ami"
+
+  type = object ({
+    name = string
+    owner = string
+  })
+
+  default = {
+    values = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owners = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Dev environment"
+
+  type = object ({
+    name = string
+    network_prefix = string 
+  })
+
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+
+}
+
+variable "asg_min_size" {
+  default = "min # instance in ASG" 
+  default  = 1
+}
+
+
+variable "asg_max_size" {
+  default = "max # instance in ASG" 
+  default  = 2
+}
