07ee0c0bc842f6435431153820ee427e3840a8c6
talisd <dimat1@yahoo.com>
2023-04-28 14:49:25 -0400

    added prod

---
+++ prod/main.tf
@@ -0,0 +1,10 @@
+module "prod" {
+    source = "../modules/blog"
+        environment = {
+        name = "prod"
+        network_prefix = "10.1"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
+}
