104363045741c9448eed8570a8f0de9e3471236b
talisd <dimat1@yahoo.com>
2023-04-28 13:05:43 -0400

    auto scale

--- main.tf
+++ main.tf
@@ -43,20 +43,26 @@ module "blog_sg" {
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  subnet_id     = module.blog_vpc.public_subnets[0]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.9.0"
+  # insert the 1 required variable here
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_identifier   = module.blog_vpc.public_subnets
+  target_group_arns     = module.blog_alb.target_group_arns
+  security_groups       = [module.blog_sg.security_group_id]
+
+  image_id           = data.aws_ami.app_ami.id
+  instance_type      = var.instance_type
 }
 
 
-module "alb" {
+
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
